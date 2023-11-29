85fe8508bf45721e0a442dc9ad7cb29219fd73ef
The-best-cse <y.mehmet.cse@gmail.com>
2023-11-29 21:11:13 +0100

    todo layout design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 import TodoList from '@/components/TodoList.vue'
+import NewTodo from '@/components/NewTodo.vue'
+import FiliterTodoList from '@/components/FiliterTodoList.vue'
+
 </script>
 
 <template>
   <main>
     <!-- <TheWelcome /> -->
+    <FiliterTodoList></FiliterTodoList>
     <TodoList></TodoList>
+    <NewTodo></NewTodo>
   </main>
 </template>
